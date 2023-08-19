65156de4ed03fcda566383110b878416cd62cef4
Maximilian Lutz <max.lutz98@me.com>
2023-08-19 22:32:21 +0200

    Updated terraform proxmox provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "2.7.4"
+      version = "2.9.11"
     }
   }
 }
@@ -11,5 +11,5 @@ provider "proxmox" {
   pm_api_url = var.api_url
   pm_api_token_id = var.token_id
   pm_api_token_secret = var.token_secret
-  pm_tls_insecure = true # Change to false if you have your 
+  pm_tls_insecure = true # Change to false if you have your
 }
