1399d94556343f13d7b37eea486231843cdd7544
Maximilian Lutz <max.lutz98@me.com>
2026-04-05 10:12:47 +0200

    Formatted tofu code

--- tofu/modules/vm/main.tf
+++ tofu/modules/vm/main.tf
@@ -9,7 +9,7 @@ locals {
 resource "proxmox_virtual_environment_vm" "vms" {
   name      = var.vm_name
   node_name = var.vm_target_node
-  vm_id = var.vm_id
+  vm_id     = var.vm_id
 
   stop_on_destroy = true
 
@@ -26,17 +26,17 @@ resource "proxmox_virtual_environment_vm" "vms" {
     # that does not have the qemu-guest-agent installed, you may need to disable the `agent` below and remove the `vm_ipv4_address` output.
     # See https://registry.terraform.io/providers/bpg/proxmox/latest/docs/resources/virtual_environment_vm#qemu-guest-agent for more details.
     enabled = true
-    trim = true
+    trim    = true
   }
 
   cpu {
     cores = var.vm_cpu_cores
-    type = "host"
+    type  = "host"
   }
 
   memory {
     dedicated = var.vm_memory
-    floating   = var.vm_balloon
+    floating  = var.vm_balloon
   }
 
   network_device {
@@ -65,17 +65,17 @@ resource "proxmox_virtual_environment_vm" "vms" {
 
     user_account {
       # keys     = [trimspace(data.local_file.ssh_public_key.content)]
-      keys     = [trimspace(file("${var.vm_ssh_key_file}"))]
+      keys = [trimspace(file("${var.vm_ssh_key_file}"))]
     }
   }
 }
 
 resource "proxmox_download_file" "latest_alpine_3_23_3_qcow2" {
-  content_type       = "import"
-  datastore_id       = "local"
-  file_name          = "generic_alpine-3.23.3-x86_64-bios-cloudinit-r0.qcow2"
-  node_name          = var.vm_target_node
-  url                = "https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/cloud/generic_alpine-3.23.3-x86_64-bios-cloudinit-r0.qcow2"
+  content_type = "import"
+  datastore_id = "local"
+  file_name    = "generic_alpine-3.23.3-x86_64-bios-cloudinit-r0.qcow2"
+  node_name    = var.vm_target_node
+  url          = "https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/cloud/generic_alpine-3.23.3-x86_64-bios-cloudinit-r0.qcow2"
 }
 
 resource "proxmox_download_file" "latest_debian_13_trixie_qcow2" {
